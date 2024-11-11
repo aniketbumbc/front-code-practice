e894486b1fa4b69c2b22ec0f5e0cdda38a7cba4f
aniket <aniket.umbc@gmail.com>
2024-11-11 13:24:33 +0530

    slice with substring

--- src/Basic code/Nov-10-24.js
+++ src/Basic code/Nov-10-24.js
@@ -28,4 +28,50 @@ function displayChoice(colors, reOrgRanks) {
   }
 }
 
-displayChoice(colors, reOrgRanks);
+//displayChoice(colors, reOrgRanks);
+
+/**
+ *
+ * Write a JavaScript function to find the longest common starting substring in a set of strings.
+ * console.log(longest_common_starting_substring(['go', 'google']));
+ *
+ */
+
+const longest_common_starting_substring = ['Go', 'Google'];
+
+function largestCommonSubstring(arry) {
+  const eleFirst = arry[0];
+  const eleSecond = arry[1];
+  let longEle = '';
+
+  for (let i = 0; i < eleSecond.length; i++) {
+    if (eleSecond[i] === eleFirst[i]) {
+      longEle += eleFirst[i];
+    } else {
+      break;
+    }
+  }
+  console.log(longEle);
+}
+
+largestCommonSubstring(longest_common_starting_substring);
+
+/**
+ *
+ * Write a JavaScript function to convert an object into a list of `[key, value]` pairs.
+ *
+ */
+
+const colorsObj = { red: '#FF0000', green: '#00FF00', white: '#FFFFFF' };
+
+function convertKeyValue(obj) {
+  const tempArray = [];
+
+  for (let key in obj) {
+    tempArray.push([key, obj[key]]);
+  }
+
+  console.log(tempArray);
+}
+
+convertKeyValue(colorsObj);
--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 // import './Basic code/Nov-9-24';
-import './Basic code/Nov-10-24';
+// import './Basic code/Nov-10-24';
+
+import './Basic code/Nov-11-24';
