47932fadbc8982766b478f74cde361421b9ee840
aniket <aniket.umbc@gmail.com>
2024-11-12 05:56:37 +0530

    object length and delete

--- src/Basic code/Nov-11-24.js
+++ src/Basic code/Nov-11-24.js
@@ -18,3 +18,33 @@ function subSetString(str) {
 }
 
 subSetString('abcd');
+
+/**
+ * Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+ Sample object:
+
+ * 
+ */
+
+const student = {
+  name: 'David Rayy',
+  sclass: 'VI',
+  rollno: 12,
+};
+
+const deletePropertyObj = (inputObj) => {
+  const cpInputObj = { ...inputObj };
+  const objLength = Object.entries(inputObj).length;
+
+  for (let ele in cpInputObj) {
+    if (ele === 'rollno') {
+      console.log('delete');
+      delete cpInputObj[ele];
+    }
+  }
+
+  console.log(cpInputObj);
+  console.log('objLength', objLength);
+};
+
+deletePropertyObj(student);
