f0a715ae706da7d7f43938345de2a0d4ea832494
aniket <aniket.umbc@gmail.com>
2024-11-15 18:26:00 +0530

    throtlling and debounce

---
+++ src/ui-code/debounce/main.js
@@ -0,0 +1,54 @@
+const input = document.querySelector('input');
+const defaultText = document.getElementById('default');
+const debounceText = document.getElementById('debounce');
+const throttlingText = document.getElementById('throttle');
+
+const updateDebounceText = debounce((text) => {
+  debounceText.textContent = text;
+});
+
+const updateThrottleText = throttle((text) => {
+  throttlingText.textContent = text;
+});
+
+input.addEventListener('input', (e) => {
+  defaultText.textContent = e.target.value;
+  updateDebounceText(e.target.value);
+  updateThrottleText(e.target.value);
+});
+
+function debounce(cb, delay = 2000) {
+  let timeout;
+
+  return (...args) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      cb(...args);
+    }, delay);
+  };
+}
+
+function throttle(cd, delay = 2000) {
+  let showText = false;
+  let waitingArgs;
+
+  const timeoutFunc = () => {
+    if (waitingArgs == null) {
+      showText = false;
+    } else {
+      cd(...waitingArgs);
+      waitingArgs = null;
+      setTimeout(timeoutFunc, delay);
+    }
+  };
+
+  return (...args) => {
+    if (showText) {
+      waitingArgs = args;
+      return;
+    }
+    cd(...args);
+    showText = true;
+    setTimeout(timeoutFunc, delay);
+  };
+}
