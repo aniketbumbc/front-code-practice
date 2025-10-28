aef9e1ed181b85453f57180874bf0dafc9c97a96
aniketbumbc <aniket.umbc@gmail.com>
2025-10-28 12:50:02 +0530

    move zero

--- src/Code Practice/Arrays/practice-session-1.js
+++ src/Code Practice/Arrays/practice-session-1.js
@@ -42,14 +42,66 @@ const removedElement = (nums, val) => {
  */
 
 const reverseInPlace = (arr) => {
-  let j = arr.length;
+  let n = arr.length;
 
-  for (let i = 0; i < Math.floor(j / 2); i++) {
+  for (let i = 0; i < Math.floor(n / 2); i++) {
     let temp = arr[i];
-    arr[i] = arr[j - 1 - i];
-    arr[j - 1 - i] = temp;
+    arr[i] = arr[n - 1 - i];
+    arr[n - 1 - i] = temp;
   }
   console.log(arr);
 };
 
-reverseInPlace(['a', 'n', 'i', 'k', 'e', 't']);
+//reverseInPlace(['a', 'n', 'i', 'k', 'e', 't']);
+
+/**
+ *  Best time to sell the stocks
+ */
+
+const bestTimeToSell = (arr) => {
+  let min = arr[0];
+  let currentProfit = 0;
+  let maxProfit = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    currentProfit = arr[i] - min;
+
+    if (currentProfit > maxProfit) {
+      maxProfit = currentProfit;
+    }
+
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+
+  return maxProfit;
+};
+
+console.log(bestTimeToSell([7, 1, 5, 3, 6, 4]));
+
+/**
+ *  Move all zeros
+ */
+
+const moveZero = (arr) => {
+  let j = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      let temp = arr[i];
+      arr[j] = temp;
+      j = j + 1;
+    }
+  }
+
+  console.log(arr);
+
+  for (let k = j; k < arr.length; k++) {
+    arr[k] = 0;
+  }
+
+  console.log(arr);
+};
+
+moveZero([0, 1, 0, 3, 12]);
