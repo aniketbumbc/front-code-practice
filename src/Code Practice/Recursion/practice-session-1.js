9518113d63ae5a21f4d61cae5ca6a39ec6f81c88
aniketbumbc <aniket.umbc@gmail.com>
2025-11-01 12:21:16 +0530

    factorial

--- src/Code Practice/Recursion/practice-session-1.js
+++ src/Code Practice/Recursion/practice-session-1.js
@@ -30,4 +30,40 @@ const printnRecursionN = (n) => {
   printnRecursionN(n + 1);
 };
 
-printnRecursionN(1);
+// printnRecursionN(1);
+
+/**
+ *  sum of number 1... n
+ */
+
+const sumN = (n) => {
+  if (n == 0) return 0;
+
+  return n + sumN(n - 1);
+};
+
+const result = sumN(5);
+
+console.log(result);
+
+const arr = [5, 3, 2, 0, 1];
+
+const sumArr = (n) => {
+  if (n == 0) return arr[n];
+
+  return arr[n] + sumArr(n - 1);
+};
+
+// console.log(sumArr(arr.length - 1));
+
+/**
+ *  Factorial Number
+ */
+
+const facto = (n) => {
+  if (n === 0) return 1;
+
+  return facto(n - 1) * n;
+};
+
+console.log(facto(5));
