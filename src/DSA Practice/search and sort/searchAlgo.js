5c91f519d7b473c14186f647b6dc3934f414877c
aniketbumbc <aniket.umbc@gmail.com>
2025-11-24 12:07:38 +0530

    spacing clear

--- src/DSA Practice/search and sort/searchAlgo.js
+++ src/DSA Practice/search and sort/searchAlgo.js
@@ -70,10 +70,8 @@ console.log(bubbleSort([5, 4, 1, 8, 2, 9, 10]));
 
 const selectionSort = (arr) => {
   const n = arr.length;
-
   for (let i = 0; i < n - 1; i++) {
     let minIndex = i;
-
     for (let j = i + 1; j < n; j++) {
       if (arr[j] < arr[minIndex]) {
         minIndex = j;
