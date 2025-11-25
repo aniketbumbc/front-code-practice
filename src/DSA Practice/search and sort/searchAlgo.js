b676069a7d14598c1b5d60bb0f5770becd936343
aniketbumbc <aniket.umbc@gmail.com>
2025-11-25 11:18:27 +0530

    insertion sort

--- src/DSA Practice/search and sort/searchAlgo.js
+++ src/DSA Practice/search and sort/searchAlgo.js
@@ -88,3 +88,28 @@ const selectionSort = (arr) => {
 
 console.log(selectionSort([5, 4, 8, 2, 9, 1]));
 console.log(selectionSort([5, 4, 1, 8, 2, 9, 10]));
+
+/**
+ *  Insertion sort
+ */
+
+const insertionSort = (arr) => {
+  const n = arr.length;
+
+  for (let i = 1; i < n; i++) {
+    let curr = arr[i];
+    let prev = i - 1;
+
+    while (arr[prev] > curr && prev >= 0) {
+      arr[prev + 1] = arr[prev];
+      prev--;
+    }
+
+    arr[prev + 1] = curr;
+  }
+
+  console.log(arr);
+};
+
+insertionSort([3, 2, 1]);
+//insertionSort([5, 4, 1, 8, 2, 9, 10]);
