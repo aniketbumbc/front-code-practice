167c98172923755557b7975b4d3c0d4539d0acbd
aniketbumbc <aniket.umbc@gmail.com>
2025-12-08 13:08:21 +0530

    has cycle in linklist

--- src/DSA Practice/Linklist/liknList-practice.js
+++ src/DSA Practice/Linklist/liknList-practice.js
@@ -33,3 +33,40 @@ function reverseList(head) {
   head = prev;
   return head;
 }
+
+/**
+ *  Cycle Linklist find
+ *
+ */
+
+var hasCycle = function (head) {
+  let curr = head;
+  let nodeSet = new Set();
+
+  while (curr) {
+    if (nodeSet.has(curr)) {
+      return true;
+    }
+    nodeSet.add(curr);
+    curr = curr.next;
+  }
+
+  return false;
+};
+
+const twoHasCycle = (head) => {
+  if (!head) return false;
+
+  let slow = head;
+  let fast = head.next;
+
+  while (slow != fast) {
+    if (fast === null || fast.next === null) {
+      return false;
+    }
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return true;
+};
