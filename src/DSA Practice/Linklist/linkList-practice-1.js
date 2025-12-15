4431b3357e38c40a9de3350e28971b7114413740
aniketbumbc <aniket.umbc@gmail.com>
2025-12-15 10:49:00 +0530

    odd n even linklist

--- src/DSA Practice/Linklist/linkList-practice-1.js
+++ src/DSA Practice/Linklist/linkList-practice-1.js
@@ -191,3 +191,22 @@ var removeDuplicates = function (arr) {
   }
   return j + 1;
 };
+
+var oddEvenList = function (head) {
+  if (!head || !head.next) return head;
+
+  let odd = head;
+  let even = head.next;
+  let evenStart = even;
+
+  while (odd.next && even.next) {
+    odd.next = odd.next.next;
+    even.next = even.next.next;
+    odd = odd.next;
+    even = even.next;
+  }
+
+  odd.next = evenStart;
+
+  return head;
+};
