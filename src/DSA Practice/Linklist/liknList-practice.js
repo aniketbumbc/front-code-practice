06ed1cf27d23d58e1b0278265d5f94f744ea5201
aniketbumbc <aniket.umbc@gmail.com>
2025-12-24 12:41:02 +0530

    recursive using swapPairs

--- src/DSA Practice/Linklist/liknList-practice.js
+++ src/DSA Practice/Linklist/liknList-practice.js
@@ -108,3 +108,19 @@ var swapPairs = function (head) {
 
   return dummy.next;
 };
+
+/**
+ *  swapPairs using recursive pattern
+ */
+
+var swapPairs = function (head) {
+  if (!head || !head.next) return head;
+
+  let l = head;
+  let r = head.next;
+
+  l.next = swapPairs(r.next);
+  r.next = l;
+
+  return r;
+};
