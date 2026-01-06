f9b138229ba2aee92ee148b23787fea219178d9d
aniketbumbc <aniket.umbc@gmail.com>
2026-01-06 10:52:32 +0530

    link list practice

--- src/DSA Practice/Linklist/revesion/5.01.26.js
+++ src/DSA Practice/Linklist/revesion/5.01.26.js
@@ -13,8 +13,8 @@ var MyLinkedList = function () {
  * @return {number}
  */
 MyLinkedList.prototype.get = function (index) {
-  if (index < 0 || index > this.size) {
-    return null;
+  if (index < 0 || index >= this.size) {
+    return -1;
   }
 
   let curr = this.head;
@@ -42,6 +42,12 @@ MyLinkedList.prototype.addAtHead = function (val) {
  */
 MyLinkedList.prototype.addAtTail = function (val) {
   let newNode = new Node(val);
+  if (!this.head) {
+    this.head = newNode;
+    this.size++;
+    return;
+  }
+
   let curr = this.head;
   while (curr.next != null) {
     curr = curr.next;
@@ -57,10 +63,12 @@ MyLinkedList.prototype.addAtTail = function (val) {
  * @return {void}
  */
 MyLinkedList.prototype.addAtIndex = function (index, val) {
+  if (index < 0 || index > this.size) return;
+
   let newNode = new Node(val);
 
-  if (!this.head) {
-    this.addAtHead(newNode);
+  if (index === 0) {
+    this.addAtHead(val);
     return;
   } else if (this.size === index) {
     this.addAtTail(val);
@@ -72,16 +80,33 @@ MyLinkedList.prototype.addAtIndex = function (index, val) {
     }
     newNode.next = curr.next;
     curr.next = newNode;
+    this.size += 1;
   }
-
-  this.size += 1;
 };
 
 /**
  * @param {number} index
  * @return {void}
  */
-MyLinkedList.prototype.deleteAtIndex = function (index) {};
+MyLinkedList.prototype.deleteAtIndex = function (index) {
+  if (index < 0 || index >= this.size) {
+    return;
+  }
+
+  if (index === 0) {
+    this.head = this.head.next;
+  } else {
+    let curr = this.head;
+
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
+    }
+
+    curr.next = curr.next.next;
+  }
+
+  this.size -= 1;
+};
 
 /**
  * Your MyLinkedList object will be instantiated and called as such:
@@ -92,3 +117,30 @@ MyLinkedList.prototype.deleteAtIndex = function (index) {};
  * obj.addAtIndex(index,val)
  * obj.deleteAtIndex(index)
  */
+
+/**
+ *  Find middle element in linklist
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function (head) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+
+  return slow;
+};
