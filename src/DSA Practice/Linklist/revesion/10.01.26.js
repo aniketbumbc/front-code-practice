60a99a1cd98dd4ce1b8dd6e7e80694bd3fac1428
aniketbumbc <aniket.umbc@gmail.com>
2026-01-10 13:42:37 +0530

    linklist removed duplicate and odd n event

---
+++ src/DSA Practice/Linklist/revesion/10.01.26.js
@@ -0,0 +1,90 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  let curr = head;
+
+  while (curr && curr.next) {
+    if (curr.val === curr.next.val) {
+      curr.next = curr.next.next;
+    } else {
+      curr = curr.next;
+    }
+  }
+  return head;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+  if (!head || !head.next) return head;
+
+  let odd = head;
+  let even = head.next;
+  let eventStart = even;
+
+  while (odd.next && even.next) {
+    odd.next = odd.next.next;
+    even.next = even.next.next;
+    odd = odd.next;
+    even = even.next;
+  }
+
+  odd.next = eventStart;
+
+  return head;
+};
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  let start = new ListNode();
+  let curr = start;
+
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+
+    curr = curr.next;
+  }
+  if (!l1) {
+    curr.next = l2;
+  }
+
+  if (!l2) {
+    curr.next = l1;
+  }
+  return start.next;
+};
