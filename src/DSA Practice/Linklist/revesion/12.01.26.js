68f96bf63e2d8ef80d456b008e127846163a30aa
aniketbumbc <aniket.umbc@gmail.com>
2026-01-13 22:09:39 +0530

    swap node

--- src/DSA Practice/Linklist/revesion/12.01.26.js
+++ src/DSA Practice/Linklist/revesion/12.01.26.js
@@ -41,3 +41,46 @@ var rotateRight = function (head, k) {
 
   return newHead;
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+  if (!head || !head.next) return head;
+
+  let l = head;
+  let r = head.next;
+  l.next = swapPairs(r.next);
+  r.next = l;
+  return r;
+};
+
+var swapPairs = function (head) {
+  if (!head || !head.next) return head;
+
+  let dummyNode = new ListNode();
+  dummyNode.next = head;
+  let p = dummyNode;
+  let c = head;
+  let n = head.next;
+
+  while (c && n) {
+    p.next = n;
+    c.next = n.next;
+    n.next = c;
+
+    p = c;
+    c = p.next;
+    n = c && c.next;
+  }
+
+  return dummyNode.next;
+};
