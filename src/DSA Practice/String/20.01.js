5b96bf8d7e2b7f8c0701f433ed58092309853f06
aniketbumbc <aniket.umbc@gmail.com>
2026-01-21 12:21:00 +0530

    isomorphic

--- src/DSA Practice/String/20.01.js
+++ src/DSA Practice/String/20.01.js
@@ -48,5 +48,28 @@ const calculateFrequency = (str) => {
   return tempObj;
 };
 
-console.log(isAnagram('anagram', 'nagaram'));
-console.log(isAnagram('rat', 'cat'));
+// console.log(isAnagram('anagram', 'nagaram'));
+// console.log(isAnagram('rat', 'cat'));
+
+/**
+ *  Isomorphic of string
+ */
+
+const isIsomorphic = (s, t) => {
+  let mapStoT = {};
+  let mapTtoS = {};
+
+  for (let i = 0; i < s.length; i++) {
+    if (!mapStoT[s[i]] && !mapTtoS[t[i]]) {
+      mapStoT[s[i]] = t[i];
+      mapTtoS[t[i]] = s[i];
+    } else if (mapStoT[s[i]] !== t[i] || mapTtoS[t[i]] !== s[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+isIsomorphic('egg', 'add');
+isIsomorphic('foo', 'bar');
+isIsomorphic('paper', 'title');
