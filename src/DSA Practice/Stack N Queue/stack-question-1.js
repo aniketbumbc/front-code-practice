0ed3bffe0848d501eca13602c6a3d27d05bde23c
aniketbumbc <aniket.umbc@gmail.com>
2026-02-03 06:24:04 +0530

    outermost-parentheses

--- src/DSA Practice/Stack N Queue/stack-question-1.js
+++ src/DSA Practice/Stack N Queue/stack-question-1.js
@@ -44,3 +44,27 @@ MinStack.prototype.getMin = function () {
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
+
+/**
+ *  1. Remove Outermost Parentheses
+ */
+
+const removeOuterParentheses = (s) => {
+  const stack = [];
+  let ans = '';
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(') {
+      stack.push(s[i]);
+      if (stack.length > 1) {
+        ans += s[i];
+      }
+    } else {
+      if (stack.length > 1) {
+        ans += s[i];
+      }
+      stack.pop();
+    }
+  }
+  return ans;
+};
