7e6743e43f4c73d33205b4273eed34437306be79
aniketbumbc <aniket.umbc@gmail.com>
2026-02-13 11:28:37 +0530

    find the peak element

--- src/DSA Practice/binarysearch/practice-1.js
+++ src/DSA Practice/binarysearch/practice-1.js
@@ -90,3 +90,19 @@ var solution = function (isBadVersion) {
     return right;
   };
 };
+
+var findPeakElement = function (arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (arr[mid + 1] > arr[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+};
