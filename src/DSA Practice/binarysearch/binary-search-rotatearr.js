f3a96e529d8dee3f0c142c042d2ccb433ae7f585
aniketbumbc <aniket.umbc@gmail.com>
2026-02-16 13:04:43 +0530

    rotated-sorted-array

---
+++ src/DSA Practice/binarysearch/binary-search-rotatearr.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor(right - left / 2);
+    if (target === arr[mid]) {
+      return mid;
+    }
+    if (arr[left] <= arr[mid]) {
+      if (target < arr[mid] && target >= arr[left]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (target > arr[mid] && target <= arr[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return -1;
+};
