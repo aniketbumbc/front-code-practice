c6ae48635639299c61471e95a9cfeedfc57ce53d
aniketbumbc <aniket.umbc@gmail.com>
2026-02-18 11:31:45 +0530

    find-first-and-last-position-of-element-in-sorted-array

--- src/DSA Practice/binarysearch/binary-search-rotatearr.js
+++ src/DSA Practice/binarysearch/binary-search-rotatearr.js
@@ -60,3 +60,40 @@ var findMin = function (arr) {
     }
   }
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function (arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+  let ans = [-1, -1];
+
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  if (arr[left] === target) ans[0] = left;
+
+  left = 0;
+  right = arr.length - 1;
+
+  while (left < right) {
+    let mid = left + Math.ceil((right - left) / 2);
+
+    if (arr[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid;
+    }
+  }
+  if (arr[right] === target) ans[1] = right;
+  return ans;
+};
