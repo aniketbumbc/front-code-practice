20eb0f81c2558402a9d2d6450aaeedee4da93ec9
aniketbumbc <aniket.umbc@gmail.com>
2026-02-19 11:49:43 +0530

    find peak Index In Mountain Array

--- src/DSA Practice/binarysearch/practice-1.js
+++ src/DSA Practice/binarysearch/practice-1.js
@@ -106,3 +106,24 @@ var findPeakElement = function (arr) {
   }
   return left;
 };
+
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var peakIndexInMountainArray = function (arr) {
+  let l = 0;
+  let r = arr.length - 1;
+
+  while (l < r) {
+    let mid = l + Math.floor((r - l) / 2);
+
+    if (arr[mid + 1] > arr[mid]) {
+      l = mid + 1;
+    } else {
+      r = mid;
+    }
+  }
+
+  return l;
+};
