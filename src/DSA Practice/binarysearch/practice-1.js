30a88e25dbdcf7d1837e907da4898d9f962bb429
aniketbumbc <aniket.umbc@gmail.com>
2026-02-25 12:37:28 +0530

    find k close element

--- src/DSA Practice/binarysearch/practice-1.js
+++ src/DSA Practice/binarysearch/practice-1.js
@@ -159,3 +159,26 @@ var singleNonDuplicate = function (arr) {
     }
   }
 };
+
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function (arr, k, x) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let mid = left + Math.floor((right - left) / 2);
+
+    if (arr[mid + k] - x < x - arr[mid]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return arr.slice(left, left + k);
+};
