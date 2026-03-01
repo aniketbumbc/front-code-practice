05b974303c5dff655ed950e76c2d071c2a2735ba
aniketbumbc <aniket.umbc@gmail.com>
2026-03-01 07:06:13 +0530

    two sum input

--- src/DSA Practice/Sliding window/practice-1.js
+++ src/DSA Practice/Sliding window/practice-1.js
@@ -23,3 +23,25 @@ var twoSum = function (arr, target) {
     tempObj[arr[i]] = i;
   }
 };
+
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (arr, target) {
+  let i = 0;
+  let j = arr.length - 1;
+
+  while (i < j) {
+    let sum = arr[i] + arr[j];
+
+    if (sum > target) {
+      j--;
+    } else if (sum < target) {
+      i++;
+    } else {
+      return [i + 1, j + 1];
+    }
+  }
+};
