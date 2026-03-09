8c0930a58fdd7f868ac2e1dbf811c06aeff7dcbf
aniketbumbc <aniket.umbc@gmail.com>
2026-03-09 12:49:20 +0530

    is sequence

--- src/DSA Practice/Sliding window/practice-1.js
+++ src/DSA Practice/Sliding window/practice-1.js
@@ -45,3 +45,17 @@ var twoSum = function (arr, target) {
     }
   }
 };
+
+var isSubsequence = function (s, t) {
+  let i = (j = 0);
+
+  while (j < t.length) {
+    if (s[i] === t[j]) {
+      i++;
+    }
+
+    j++;
+  }
+
+  return i == s.length;
+};
