08203f8713c08e4aa30bebc4b5746ee478744cb1
aniketbumbc <aniket.umbc@gmail.com>
2026-03-23 12:37:18 +0530

    max area

--- src/DSA Practice/Sliding window/practice-1.js
+++ src/DSA Practice/Sliding window/practice-1.js
@@ -100,3 +100,28 @@ var getIntersectionNode = function (headA, headB) {
 
   return Pa;
 };
+
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (arr) {
+    let i = 0;
+    let j = arr.length - 1;
+    let maxWater = 0;
+
+    while (i < j) {
+        let area = Math.min(arr[i], arr[j]) * (j - i);
+        maxWater = Math.max(area, maxWater);
+
+        if (arr[i] > arr[j]) {
+            j--;
+        } else {
+            i++;
+        }
+    }
+
+    return maxWater;
+
+};
