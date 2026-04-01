08039d86a8f699533fe73d4e4f41bebf04604337
aniketbumbc <aniket.umbc@gmail.com>
2026-04-01 13:27:33 +0530

    sliding-window-maximum

--- src/DSA Practice/Sliding window/practice-1.js
+++ src/DSA Practice/Sliding window/practice-1.js
@@ -168,13 +168,13 @@ const checkInclusion = (s1, s2) => {
   let j = window_size - 1;
 
   while (j < s2.length) {
-    if (hasSameHash(freq1, freq2)) {
+    if (hasSameHash(freqS1, freqS2)) {
       return true;
     } else {
-      freqS2[s2.charCodeAt(i) - 97]--; // removed from 0 positoin
-      i++; // i move  0 position to 1 position
-      j++; // j move to 1 position right
-      freqS2[s2.charCodeAt(j) - 97]++; // check j position charcode
+      freqS2[s2.charCodeAt(i) - 97]--; // removed from 0 position
+      i++; // i move  0 position to 1 position right side move
+      j++; // j move to 1 position right more right side move
+      freqS2[s2.charCodeAt(j) - 97]++; // check j position char code
     }
   }
 
@@ -191,3 +191,81 @@ const hasSameHash = (freqS1, freqS2) => {
 };
 
 checkInclusion('ab', 'eidbaooo');
+
+var checkInclusion = function (s1, s2) {
+  if (s1.length > s2.length) return false;
+
+  let s1Count = {};
+  let s2Count = {};
+
+  // initial window
+  for (let k = 0; k < s1.length; k++) {
+    s1Count[s1[k]] = (s1Count[s1[k]] || 0) + 1;
+    s2Count[s2[k]] = (s2Count[s2[k]] || 0) + 1;
+  }
+
+  if (isEqual(s1Count, s2Count)) return true;
+
+  let i = 0; // left pointer
+  let j = s1.length; // right pointer (next index to include)
+
+  while (j < s2.length) {
+    // add new char (right side)
+    s2Count[s2[j]] = (s2Count[s2[j]] || 0) + 1;
+
+    // remove old char (left side)
+    s2Count[s2[i]]--;
+    if (s2Count[s2[i]] === 0) {
+      delete s2Count[s2[i]];
+    }
+
+    i++;
+    j++;
+
+    if (isEqual(s1Count, s2Count)) return true;
+  }
+
+  return false;
+};
+
+function isEqual(obj1, obj2) {
+  if (Object.keys(obj1).length !== Object.keys(obj2).length) {
+    return false;
+  }
+  for (let key in obj1) {
+    if (obj1[key] !== obj2[key]) return false;
+  }
+  return true;
+}
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (arr, k) {
+  let result = [];
+  let queue = [];
+  let i = (j = 0);
+
+  while (j < arr.length) {
+    while (queue.length && arr[j] > queue[queue.length - 1]) {
+      queue.pop(); // removed all small element than current element.
+    }
+    queue.push(arr[j]);
+
+    // when jis on window size
+    if (j >= k - 1) {
+      result.push(queue[0]);
+
+      // monotonic decreasing deque if left most element of current window arr[i] is greater queue[0] element then removed that
+      //If arr[i] is equal to queue[0] (the current maximum), and you don’t remove it: You’ll keep using an element that is no longer in the window//
+      if (arr[i] === queue[0]) {
+        queue.shift();
+      }
+      i++;
+    }
+    j++;
+  }
+  return result;
+};
