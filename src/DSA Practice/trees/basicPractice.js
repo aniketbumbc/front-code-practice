b5d0cbe7ac44e822e75bfb97e285695d65249487
aniketbumbc <aniket.umbc@gmail.com>
2026-04-08 13:15:38 +0530

    inorder and postorder with tress traversal

--- src/DSA Practice/trees/basicPractice.js
+++ src/DSA Practice/trees/basicPractice.js
@@ -24,3 +24,28 @@ var preorderTraversal = function (root) {
 
   return ans;
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  const ans = [];
+
+  const travel = (curr) => {
+    if (!curr) return;
+    travel(curr.left); // left
+    travel(curr.right); // right
+    ans.push(curr.val); // root
+  };
+  travel(root);
+  return ans;
+};
