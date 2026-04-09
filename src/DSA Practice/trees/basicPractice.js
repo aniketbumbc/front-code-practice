2ca551dc3ca63f54d5cb86103d38deeb238c32ea
aniketbumbc <aniket.umbc@gmail.com>
2026-04-09 11:26:30 +0530

    iterative approach of traversal

--- src/DSA Practice/trees/basicPractice.js
+++ src/DSA Practice/trees/basicPractice.js
@@ -49,3 +49,50 @@ var postorderTraversal = function (root) {
   travel(root);
   return ans;
 };
+
+var postorderTraversal = function (root) {
+  if (!root) return [];
+  const s1 = [root];
+  const s2 = [];
+  const ans = [];
+
+  while (s1.length) {
+    curr = s1.pop();
+    s2.push(curr.val);
+    curr.left && s1.push(curr.left);
+    curr.right && s1.push(curr.right);
+  }
+
+  // while (s2.length) {
+  //     ans.push(s2.pop().val);
+  // }
+  return s2.reverse();
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root) {
+  if (!root) return [];
+  let ans = [];
+  const stack = [root];
+
+  while (stack.length) {
+    //root --- left -- right
+    const curr = stack.pop();
+
+    ans.push(curr.val); // root
+    curr.right && stack.push(curr.right); // to save right for reference
+    curr.left && stack.push(curr.left); // this what algo say r
+  }
+  return ans;
+};
