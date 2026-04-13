13a2e7e75b45729758937493f471bcbfcac9677a
aniketbumbc <aniket.umbc@gmail.com>
2026-04-13 12:41:28 +0530

    maxDepth binary tree

--- src/DSA Practice/trees/problembfsdsf.js
+++ src/DSA Practice/trees/problembfsdsf.js
@@ -31,3 +31,33 @@ var levelOrder = function (root) {
 
   return ans;
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+    if (!root) return 0;
+    let maxDepth = 0;
+
+    const travel = (curr, depth) => {
+        maxDepth = Math.max(depth, maxDepth);
+        curr.left && travel(curr.left, depth + 1);
+        curr.right && travel(curr.right, depth + 1);
+
+    }
+
+    travel(root, 1);
+
+    return maxDepth;
+
+};
