95dcdeabc2cf52dada2c5a3a4d16d0d6a7e35683
aniketbumbc <aniket.umbc@gmail.com>
2026-04-15 11:12:30 +0530

    path sum

--- src/DSA Practice/trees/problembfsdsf.js
+++ src/DSA Practice/trees/problembfsdsf.js
@@ -61,3 +61,40 @@ var maxDepth = function (root) {
     return maxDepth;
 
 };
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+
+    if (!root) return [];
+    let ans = false
+
+
+    const travel = (curr, currSum) => {
+
+        let sum = curr.val + currSum;
+
+        if (!curr.left && !curr.right) {
+            if (sum === targetSum) {
+                ans = ans || true
+            }
+        }
+
+        curr.left && travel(curr.left, sum);
+        curr.right && travel(curr.right, sum);
+
+    }
+    travel(root, 0);
+    return ans;
+};
