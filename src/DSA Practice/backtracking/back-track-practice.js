eaa31b3646e7b564fde3c1782ba83316dc66b398
aniketbumbc <aniket.umbc@gmail.com>
2026-07-07 06:30:38 +0530

    permutation n backtrack

--- src/DSA Practice/backtracking/back-track-practice.js
+++ src/DSA Practice/backtracking/back-track-practice.js
@@ -43,3 +43,28 @@ var combine = function (n, k) {
   backtrack([], 1);
   return result;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+  let result = [];
+
+  const backtrack = (path) => {
+    if (path.length == nums.length) {
+      result.push([...path]);
+      return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      if (!path.includes(nums[i])) {
+        path.push(nums[i]);
+        backtrack(path);
+        path.pop();
+      }
+    }
+  };
+  backtrack([]);
+  return result;
+};
