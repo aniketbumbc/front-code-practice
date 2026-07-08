85f5e28d57459dcdb2c0f7cb736ecfff64989620
aniketbumbc <aniket.umbc@gmail.com>
2026-07-08 07:03:11 +0530

    combination sum using backtrack

--- src/DSA Practice/backtracking/back-track-practice.js
+++ src/DSA Practice/backtracking/back-track-practice.js
@@ -68,3 +68,26 @@ var permute = function (nums) {
   backtrack([]);
   return result;
 };
+
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function (arr, target) {
+  const result = [];
+  const backtrack = (remainSum, path, start) => {
+    if (remainSum === 0) {
+      result.push([...path]);
+    }
+    if (remainSum <= 0) return;
+
+    for (let i = start; i < arr.length; i++) {
+      path.push(arr[i]);
+      backtrack(remainSum - arr[i], path, i);
+      path.pop();
+    }
+  };
+  backtrack(target, [], 0);
+  return result;
+};
